2958694e01c13c776041fd1ec64cfe1b50599084
ivan reinaldo <squall313390@yahoo.co.id>
2015-12-22 18:55:27 +0700

    aw

--- app/src/main/java/com/shineapptpa/rei/shine/ShineUser.java
+++ app/src/main/java/com/shineapptpa/rei/shine/ShineUser.java
@@ -4,5 +4,8 @@ package com.shineapptpa.rei.shine;
  * Created by rei on 12/22/2015.
  */
 public class ShineUser {
-
+        private static String currentUser;
+        private static String currentUserSchool;
+        private String username;
+        private String school;
 }
